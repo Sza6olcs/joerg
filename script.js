00b8e77bf2372bf100411d1250e9da9df3ce321b
Szabolcs Balázs <szabolcs.balazs@outlook.com>
2025-01-21 12:52:18 +0100

    added

---
+++ script.js
@@ -0,0 +1,37 @@
+function saveContact() {
+  // Kapcsolati információk definiálása
+  var contact = {
+    name: "Jörg Becker",
+    title: "Business Unit Director",
+    email: "joerg.becker@inducat.com",
+    mobile: "+4915146346883",
+    company: "Inducat GTS." };
+
+  // vCard string létrehozása
+  var vCardData = `BEGIN:VCARD
+VERSION:3.0
+FN:${contact.name}
+TITLE:${contact.title}
+EMAIL:${contact.email}
+TEL;TYPE=CELL:${contact.mobile}
+ORG:${contact.company}
+END:VCARD`;
+
+  // Blob létrehozása a vCard adatokból
+  var blob = new Blob([vCardData], { type: "text/vcard;charset=utf-8" });
+
+  // Link elem létrehozása
+  var link = document.createElement("a");
+
+  // Blob URL beállítása és letöltési attribútum
+  link.href = URL.createObjectURL(blob);
+  link.download = `${contact.name}.vcf`;
+
+  // Link hozzáadása a dokumentumhoz, kattintás szimulálása és eltávolítása
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+  // Az objektum URL visszavonása
+  URL.revokeObjectURL(link.href);
+}
